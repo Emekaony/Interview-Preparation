f0383ebf50d930f7008d75d6d22dbe48d7a22a7f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-03-07 00:47:09 -0800

    Wrote better comments for different pop edge cases

--- alg&ds/linkedList/LinkedListConstructor.js
+++ alg&ds/linkedList/LinkedListConstructor.js
@@ -23,29 +23,23 @@ class LinkedList {
     return this;
   }
   pop() {
-    // if there's no Node, then return undefined
+    // if there's nothing in the LinkedList, return undefined. Can't pop what doesn't exist
     if (!this.head) return undefined;
 
-    // use the two pointer strategy to traverse the LinkedList
+    // use the two pointer strategy to traverse the LL till you arrive at the last one
     let temp = this.head;
     let pre = this.head;
 
-    // this will happen until we get to the last Node where temp.next === null
     while (temp.next) {
       pre = temp;
       temp = temp.next;
     }
 
-    // we must have gotten to the last Node in the LinkedList, this is the Node we want to pop off of the List
-    // we set the tail to pre, the one before temp
     this.tail = pre;
-    // then we set the tail's next property to null, this is where we remove the linke between
-    // the second to last and the last Node in the LL
     this.tail.next = null;
-    // decrement the length of the LL because we just popped something off of it
     this.length--;
 
-    // what if we had just 1 item in our LL??
+    // One more Node left, so just set head and tail to null
     if (this.length === 0) {
       this.head = null;
       this.tail = null;
@@ -57,8 +51,13 @@ class LinkedList {
 
 let myLinkedList = new LinkedList(4);
 myLinkedList.push(6);
-console.log(myLinkedList.pop());
+console.log(myLinkedList);
+
+myLinkedList.pop();
+console.log(myLinkedList);
+
+myLinkedList.pop();
 console.log(myLinkedList);
 
 console.log(myLinkedList.pop());
-console.log(myLinkedList.pop());
+console.log(myLinkedList);
